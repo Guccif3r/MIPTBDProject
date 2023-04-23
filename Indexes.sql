1384f3ba2094cd9a91b11fc76aec254bf3d4c9b6
RadixXxAlexMIPT <gavrilin@phystech.edu>
2023-04-23 13:55:25 +0300

    Clear project main

--- Indexes.sql
+++
@@ -1,7 +0,0 @@
-set search_path = workspace;
-
--- Поможет в WHERE по price/weight
-CREATE INDEX ON Products(price);
-CREATE INDEX ON Products(weight);
-CREATE INDEX ON Dishes(price);
-CREATE INDEX ON Dishes(weight);
